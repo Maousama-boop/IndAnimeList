09e35a12422c36e0d7b2dc69b46f243e0d884ce6
Blythes <145576625+Maousama-boop@users.noreply.github.com>
2024-07-02 19:21:46 +0700

    update src/app/anime/[id]/page.jsx

--- src/app/anime/[id]/page.jsx
+++ src/app/anime/[id]/page.jsx
@@ -8,7 +8,7 @@ const Page = async({params: {id}}) => {
             <div className="p-3">
                 <h3 className="text-color-primary text-2xl">{animeDetail.data.title} - {animeDetail.data.year}</h3>
             </div>
-            <div>
+            <div className="p-3">
             <Image src={} />
             </div>
         </>
