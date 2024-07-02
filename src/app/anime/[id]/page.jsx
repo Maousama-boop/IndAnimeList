0927865a5c381d391c6831121c6bbc3072ac2b79
Blythes <145576625+Maousama-boop@users.noreply.github.com>
2024-07-02 19:38:46 +0700

    update src/app/anime/[id]/page.jsx

--- src/app/anime/[id]/page.jsx
+++ src/app/anime/[id]/page.jsx
@@ -8,6 +8,7 @@ const Page = async({params: {id}}) => {
             <div className="p-3">
                 <h3 className="text-color-primary text-2xl">{animeDetail.data.title} - {animeDetail.data.year}</h3>
             </div>
+            <div className="p-3 flex gap-3 text-color-primary overflow-x-auto"></div>
             <div className="p-3 flex sm:flex-nowrap flex-wrap gap-3 text-color-primary">
             <Image src={animeDetail.data.images.webp.image_url} alt={animeDetail.data.images.jpg.image_url} height={250} width={250} className="object-cover rounded w-full"/>
             <p className="text-justify text-xl">{animeDetail.data.synopsis}</p>
