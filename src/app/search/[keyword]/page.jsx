19b2139fa1db7a2aac805ca5f21d96731b3a244e
sekar0506 <sekaranindita.0506@gmail.com>
2024-07-02 14:39:12 +0700

    Update page.jsx

--- src/app/search/[keyword]/page.jsx
+++ src/app/search/[keyword]/page.jsx
@@ -1,11 +1,11 @@
 import IndAnimeList from "../../../components/IndAnimeList"
 import Header from "../../../components/IndAnimeList/Header"
+import { getAnimeResponse } from "../../libs/api-libs"
 
 const Page = async ({ params }) => {
   const { keyword } = params
   const decodedKeyword = decodeURI(keyword)
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/anime?q=${decodedKeyword}`)
-  const searchAnime = await response.json()
+  const searchAnime = await getAnimeResponse("anime", `q=${decodedKeyword}`)
   return (
     <>
       <section>
