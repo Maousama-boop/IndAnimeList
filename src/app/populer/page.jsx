a19915a8f0f1109e95d972fc0e15ad4cef5c2a31
sekar0506 <sekaranindita.0506@gmail.com>
2024-07-02 14:35:51 +0700

    Update page.jsx

--- src/app/populer/page.jsx
+++ src/app/populer/page.jsx
@@ -4,15 +4,15 @@ import { useEffect, useState } from "react"
 import HeaderPopuler from "../../components/Utilities/HeaderPopuler"
 import Pagination from "../../components/Utilities/Pagination"
 import IndAnimeList from "../../components/IndAnimeList"
+import { getAnimeResponse } from "../libs/api-libs"
 
 const Populer = () => {
     const [page, setPage] = useState(1)
     const [topAnime, setTopAnime] = useState([])
 
     const fetchData = async() => {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?page=${page}`)
-        const data = await response.json()
-        setTopAnime(data)
+        const populerAnime = await getAnimeResponse("top/anime", `page=${page}`)
+        setTopAnime(populerAnime)
     }
 
     useEffect(() => {
