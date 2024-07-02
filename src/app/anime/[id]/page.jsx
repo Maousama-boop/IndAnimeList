cbd885a826bfdf3ca2ab0dc0668735e71b2e1949
Blythes <145576625+Maousama-boop@users.noreply.github.com>
2024-07-02 19:12:29 +0700

    update src/app/anime/[id]/page.jsx

--- src/app/anime/[id]/page.jsx
+++ src/app/anime/[id]/page.jsx
@@ -2,11 +2,10 @@ import { getAnimeResponse } from "../../libs/api-libs"
 
 const Page = async({params: {id}}) => {
     const animeDetail = await getAnimeResponse(`anime/${id}`)
-    console.log(animeDetail)
     return (
         <>
             <div className="p-3">
-                <h3 className="text-color-primary text-2xl">Anime Detail</h3>
+                <h3 className="text-color-primary text-2xl">{animeDetail.title}</h3>
             </div>
         </>
     )
