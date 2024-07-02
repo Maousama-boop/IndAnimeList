31a3fd00270e6733f76a5b6069b18526978261f6
sekar0506 <sekaranindita.0506@gmail.com>
2024-07-02 14:16:09 +0700

    Update Pagination.jsx

--- src/components/Utilities/Pagination.jsx
+++ src/components/Utilities/Pagination.jsx
@@ -18,7 +18,9 @@ const Pagination = ({ page, lastPage, setPage}) => {
     
     return (
         <div className="flex justify-center items-center py-4 px-2 gap-5 text-color-primary">
-            <button onClick={handlePrevPage} className="transition-all hover:text-color-accent">Prev</button>
+            {page <= 1 ? null :
+                <button onClick={handlePrevPage} className="transition-all hover:text-color-accent">Prev</button>
+            }
             <p>{page} Dari {lastPage}</p>
             <button onClick={handleNextPage} className="transition-all hover:text-color-accent">Next</button>
         </div>
