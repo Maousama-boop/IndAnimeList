b8d7311d581287c07d9dc563e563d01a81b5016f
Blythes <145576625+Maousama-boop@users.noreply.github.com>
2024-07-02 21:05:57 +0700

    update src/app/anime/[id]/page.jsx

--- src/app/anime/[id]/page.jsx
+++ src/app/anime/[id]/page.jsx
@@ -1,5 +1,6 @@
 import { getAnimeResponse } from "../../libs/api-libs"
 import Image from "next/image"
+import Link from "next/link"
 
 const Page = async({params: {id}}) => {
     const animeDetail = await getAnimeResponse(`anime/${id}`)
@@ -42,6 +43,9 @@ const Page = async({params: {id}}) => {
             <div className="p-3 flex sm:flex-nowrap flex-wrap gap-3 text-color-primary font-semibold">
             <Image src={animeDetail.data.images.webp.image_url} alt={animeDetail.data.images.jpg.image_url} height={250} width={250} className="object-cover rounded w-full"/>
             <p className="text-justify text-xl">{animeDetail.data.synopsis}</p>
+            </div>
+            <div>
+            <link></Link>
             </div>
         </>
     )
