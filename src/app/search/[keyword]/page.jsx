9aef083ba05a2704fa9b8785024b789e977be9be
Blythes <145576625+Maousama-boop@users.noreply.github.com>
2024-07-03 22:44:52 +0700

    update src/app/search/[keyword]/page.jsx

--- src/app/search/[keyword]/page.jsx
+++ src/app/search/[keyword]/page.jsx
@@ -6,11 +6,16 @@ const Page = async ({ params }) => {
   const { keyword } = params
   const decodedKeyword = decodeURI(keyword)
   const searchAnime = await getAnimeResponse("anime", `q=${decodedKeyword}`)
+  const hasResults = searchAnime && searchAnime.data && searchAnime.data.length > 0;
   return (
     <>
       <section>
+      {hasResults ? (
         <Header title={`Pencarian '${decodedKeyword}'`}/>
         <IndAnimeList api={searchAnime}/>
+        ) : (
+        <Header title={`Pencarian '${decodedKeyword}' Tidak Di Temukan.`}/>
+          )}
       </section>
     </>
   );
