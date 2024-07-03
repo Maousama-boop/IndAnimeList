61669cac8443b71049ca300ef1f975e08efa4d95
Blythes <145576625+Maousama-boop@users.noreply.github.com>
2024-07-03 18:25:50 +0700

    update src/app/page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,12 +4,17 @@ import { getAnimeResponse } from "./libs/api-libs"
 
 const Page = async () => {
   const topAnime = await getAnimeResponse("top/anime", "limit=10")
+  const topCharacter = await getAnimeResponse("top/characters", "limit=10")
 
   return (
     <>
       <section>
         <Header title="Paling Populer" linkTitle="Lihat Semua" linkHref="/populer"/>
         <IndAnimeList api={topAnime}/>
+      </section>
+      <section>
+        <Header title="Paling Populer" linkTitle="Lihat Semua" linkHref="/character"/>
+        <IndAnimeList api={topCharacter}/>
       </section>
     </>
   );
