00fef6986ebea7ef339948505d79bfaa85e89602
Blythes <145576625+Maousama-boop@users.noreply.github.com>
2024-07-04 19:30:17 +0700

    update src/app/anime/[id]/page.jsx

--- src/app/anime/[id]/page.jsx
+++ src/app/anime/[id]/page.jsx
@@ -14,28 +14,6 @@ const Page = async({params: {id}}) => {
                 null
                 }
             </div>
-            <div className="p-3 flex gap-3 text-color-primary overflow-x-auto capitaliz no-scrollbar text-center">
-            <div className="w-36 flex flex-col justify-center items-center rounded border-2 border-color-accent p-2">
-            <h3>Rank</h3>
-            <p>{animeDetail.data.rank}</p>
-            </div>
-            <div className="w-36 flex flex-col justify-center items-center rounded border-2 border-color-accent p-2">
-            <h3>rating</h3>
-            <p>{animeDetail.data.score}</p>
-            </div>
-            <div className="w-36 flex flex-col justify-center items-center rounded border-2 border-color-accent p-2">
-            <h3>member</h3>
-            <p>{animeDetail.data.members}</p>
-            </div>
-            <div className="w-36 flex flex-col justify-center items-center rounded border-2 border-color-accent p-2">
-            <h3>favorit</h3>
-            <p>{animeDetail.data.favorites}</p>
-            </div>
-            <div className="w-36 flex flex-col justify-center items-center rounded border-2 border-color-accent p-2">
-            <h3>Episode</h3>
-            <p>{animeDetail.data.episodes}</p>
-            </div>
-            </div>
             <div className="p-3 flex sm:flex-nowrap flex-wrap gap-3 text-color-primary font-semibold capitalize">
             <Image src={animeDetail.data.images.webp.image_url} alt={animeDetail.data.images.jpg.image_url} height={250} width={250} className="object-cover rounded w-full h-full"/>
             <p className="text-justify sm:text-base text-xl">{animeDetail.data.synopsis}</p>
