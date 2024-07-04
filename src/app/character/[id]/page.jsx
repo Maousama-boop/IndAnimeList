54909910582c8679378172dd2ec4967345ec94c9
Blythes <145576625+Maousama-boop@users.noreply.github.com>
2024-07-04 22:56:30 +0700

    update src/app/character/[id]/page.jsx

--- src/app/character/[id]/page.jsx
+++ src/app/character/[id]/page.jsx
@@ -6,7 +6,18 @@ const Page = async({params: {id}}) => {
     const animeCharacter = await getAnimeResponse(`characters/${id}`)
     return (
         <>
-            <div className="p-3 pb-0 font-semibold">Char Id {id}</div>
+            <div className="p-3 pb-0 font-semibold">
+                <h3 className="text-color-primary text-2xl">{animeCharacter.data.name}</h3>
+                {animeCharacter && animeCharacter.data.name_kanji ?
+                <h4 className="text-color-primary text-l">Kanji - {animeCharacter.data.name_kanji}</h4>
+                :
+                null
+                }
+            </div>
+            <div className="p-3 flex sm:flex-nowrap flex-wrap gap-3 text-color-primary font-semibold capitalize">
+            <Image src={animeCharacter.data.images.webp.image_url} alt={animeCharacter.data.images.jpg.image_url} height={250} width={250} className="object-cover rounded w-full h-full"/>
+            <p className="text-justify sm:text-base text-xl">{animeCharacter.data.about}</p>
+            </div>
         </>
     )
 }
