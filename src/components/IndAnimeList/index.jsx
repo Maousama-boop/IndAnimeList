d7a8630b9f2fcabde9892a04440a11c75cdad7f0
Blythes <145576625+Maousama-boop@users.noreply.github.com>
2024-07-04 18:06:05 +0700

    update src/components/IndAnimeList/index.jsx

--- src/components/IndAnimeList/index.jsx
+++ src/components/IndAnimeList/index.jsx
@@ -23,7 +23,7 @@ const IndCharacterList = ({api}) => {
         <div className="grid md:grid-cols-5 sm:grid-cols-3 grid-cols-2 gap-4 px-3">
             {api.data?.map((character, index) => {
                 return (
-                    <Link href={`/characters/${character.mal_id}`}className="relative overflow-hidden cursor-pointer text-color-primary hover:text-color-accent transition-all anime" key={index}>
+                    <Link href={`/character/${character.mal_id}`}className="relative overflow-hidden cursor-pointer text-color-primary hover:text-color-accent transition-all anime" key={index}>
                         <Image src={character.images.webp.image_url} width={350} height={350} className="w-full h-full max-h-64 md:max-h-72 object-cover"/>
                         <div className="absolute bottom-0 w-full text-center p-2 box-border overlay">
                           <h3 className="font-bold md:text-x text-md text-start  pb-0 whitespace-nowrap overflow-hidden text-ellipsis">{character.name}</h3>
