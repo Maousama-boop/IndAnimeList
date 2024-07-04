920aea857a2e6f12d74b4d0295d8f40540e942fe
Blythes <145576625+Maousama-boop@users.noreply.github.com>
2024-07-04 17:12:05 +0700

    update src/components/IndAnimeList/index.jsx

--- src/components/IndAnimeList/index.jsx
+++ src/components/IndAnimeList/index.jsx
@@ -9,7 +9,7 @@ const IndAnimeList = ({api}) => {
                     <Link href={`/anime/${anime.mal_id}`}className="relative overflow-hidden cursor-pointer text-color-primary hover:text-color-accent transition-all anime" key={index}>
                         <Image src={anime.images.webp.image_url} width={350} height={350} className="w-full h-full max-h-64 md:max-h-72 object-cover"/>
                         <div className="absolute bottom-0 w-full text-center p-2 box-border overlay">
-                          <h3 className="font-bold md:text-x text-md text-start title-anime whitespace-nowrap overflow-hidden text-ellipsis">{anime.title}</h3>
+                          <h3 className="font-bold md:text-x text-md text-start  pb-1 whitespace-nowrap overflow-hidden text-ellipsis">{anime.title}</h3>
                           <p className="whitespace-normal md:text-sm text-xs text-start">score: {anime.score}</p>
                         </div>
                     </Link>
