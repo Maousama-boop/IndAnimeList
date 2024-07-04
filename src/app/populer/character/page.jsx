b325a0070c88d5dafe35b8f4967f87c3900c7498
Blythes <145576625+Maousama-boop@users.noreply.github.com>
2024-07-04 18:59:03 +0700

    update src/app/populer/character/page.jsx

---
+++ src/app/populer/character/page.jsx
@@ -0,0 +1,31 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import HeaderPopuler from "../../components/Utilities/HeaderPopuler"
+import Pagination from "../../components/Utilities/Pagination"
+import { IndAnimeList, IndCharacterList } from "../../components/IndAnimeList"
+import { getAnimeResponse } from "../libs/api-libs"
+
+const CharacterPopuler = () => {
+    const [page, setPage] = useState(1)
+    const [topCharacter, setTopCharacter] = useState([])
+
+    const fetchData = async() => {
+        const populerCharacter = await getAnimeResponse("top/characters", `page=${page}`)
+        setTopCharacter(populerCharacter)
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [page])
+    
+    return (
+        <>
+            <HeaderPopuler title={`Character Populer #${page}`}/>
+            <IndCharacterList api={topCharacter}/>
+            <Pagination page={page} lastPage={topCharacter.pagination?.last_visible_page} setPage={setPage}/> 
+        </>
+    )
+}
+
+export default CharacterPopuler
