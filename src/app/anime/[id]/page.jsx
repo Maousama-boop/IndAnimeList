59f977a124481a7898c133b12eb16d11bbcc1a9f
Blythes <145576625+Maousama-boop@users.noreply.github.com>
2024-07-05 19:25:25 +0700

    update src/app/anime/[id]/page.jsx

--- src/app/anime/[id]/page.jsx
+++ src/app/anime/[id]/page.jsx
@@ -4,6 +4,7 @@ import Link from "next/link"
 
 const Page = async({params: {id}}) => {
     const animeDetail = await getAnimeResponse(`anime/${id}`)
+    const animeCharacter = await getAnimeResponse(`anime/${id}/characters`)
     return (
         <>
             <div className="p-3 pb-0 font-semibold">
@@ -18,6 +19,7 @@ const Page = async({params: {id}}) => {
             <Image src={animeDetail.data.images.webp.image_url} alt={animeDetail.data.images.jpg.image_url} height={250} width={250} className="object-cover rounded w-48 md:w-56 h-full"/>
             <p className="text-justify sm:text-x text-sm">{animeDetail.data.synopsis}</p>
             </div>
+            <p className="text-2xl text-color-primary">{animeCharacter.data.character.name}</p>
         </>
     )
 }
