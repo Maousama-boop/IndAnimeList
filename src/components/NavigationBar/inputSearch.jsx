660d56ab1046a5d3ead996ec1ab1d99efc3452bb
Blythes <145576625+Maousama-boop@users.noreply.github.com>
2024-07-05 23:15:17 +0700

    update src/components/NavigationBar/inputSearch.jsx

--- src/components/NavigationBar/inputSearch.jsx
+++ src/components/NavigationBar/inputSearch.jsx
@@ -27,11 +27,11 @@ const InputSearch = () => {
              <button className="absolute top-2 end-2" onClick={handleSearch}>
                 <MagnifyingGlass size={24} />
              </button>
-             {searchRef.current.value && (
+
              <button className="absolute top-2 end-10" onClick={hapusPencarian}>
                 <X size={24} />
              </button>
-             )}
+
         </div>
     )
 }
