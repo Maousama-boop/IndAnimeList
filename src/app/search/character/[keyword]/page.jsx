f43112d4cb3947a32038c042c8e53deab5fd3256
Blythes <145576625+Maousama-boop@users.noreply.github.com>
2024-07-06 21:02:33 +0700

    update src/app/search/character/[keyword]/page.jsx

---
+++ src/app/search/character/[keyword]/page.jsx
@@ -0,0 +1,19 @@
+import { IndAnimeList, IndCharacterList } from "../../../../components/IndAnimeList"
+import Header from "../../../../components/IndAnimeList/Header"
+import { getAnimeResponse } from "../../../libs/api-libs"
+
+const Page = async ({ params }) => {
+  const { keyword } = params
+  const decodedKeyword = decodeURI(keyword)
+  const searchAnime = await getAnimeResponse("characters", `q=${decodedKeyword}`)
+  return (
+    <>
+      <section>
+        <Header title={`Pencarian Character '${decodedKeyword}'`}/>
+        <IndAnimeList api={searchAnime}/>
+      </section>
+    </>
+  );
+}
+
+export default Page
