846b5cbfefa8906110d9c91971645136d7d7bf47
Blythes <145576625+Maousama-boop@users.noreply.github.com>
2024-07-06 21:05:34 +0700

    update src/app/search/character/[keyword]/page.jsx

--- src/app/search/character/[keyword]/page.jsx
+++ src/app/search/character/[keyword]/page.jsx
@@ -5,12 +5,12 @@ import { getAnimeResponse } from "../../../libs/api-libs"
 const Page = async ({ params }) => {
   const { keyword } = params
   const decodedKeyword = decodeURI(keyword)
-  const searchAnime = await getAnimeResponse("characters", `q=${decodedKeyword}`)
+  const searchCharacter = await getAnimeResponse("characters", `q=${decodedKeyword}`)
   return (
     <>
       <section>
         <Header title={`Pencarian Character '${decodedKeyword}'`}/>
-        <IndCharacterList api={searchAnime}/>
+        <IndCharacterList api={searchCharacter}/>
       </section>
     </>
   );
