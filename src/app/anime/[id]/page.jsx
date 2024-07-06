b6e9f16e83b220f32270fe51dec65e3f840abc75
Blythes <145576625+Maousama-boop@users.noreply.github.com>
2024-07-06 21:21:47 +0700

    update src/app/anime/[id]/page.jsx

--- src/app/anime/[id]/page.jsx
+++ src/app/anime/[id]/page.jsx
@@ -15,7 +15,7 @@ const Page = async({params: {id}}) => {
                 }
             </div>
             <div className="p-3 flex justify-center items-center sm:flex-nowrap flex-wrap gap-3 text-color-primary font-semibold capitalize">
-            <Image src={animeDetail.data.images.webp.image_url} alt={animeDetail.data.images.jpg.image_url} height={250} width={250} className="object-cover rounded w-48 md:w-56 h-full"/>
+            <Image src={animeDetail.data.images.webp.image_url} alt={animeDetail.data.images.jpg.image_url} height={250} width={250} className="object-cover rounded-md w-48 md:w-56 h-full"/>
             <p className="text-justify sm:text-x text-sm">{animeDetail.data.synopsis}</p>
             </div>
         </>
