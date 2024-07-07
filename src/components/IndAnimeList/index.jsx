581e4ab6516051b857a366d6f0b7a8adb85a181f
Blythes <145576625+Maousama-boop@users.noreply.github.com>
2024-07-07 11:21:40 +0700

    update src/components/IndAnimeList/index.jsx

--- src/components/IndAnimeList/index.jsx
+++ src/components/IndAnimeList/index.jsx
@@ -7,7 +7,7 @@ const IndAnimeList = ({api}) => {
             {api.data?.map((anime, index) => {
                 return (
                     <Link href={`/anime/${anime.mal_id}`}className="relative overflow-hidden cursor-pointer text-color-primary hover:text-color-accent transition-all anime rounded-md" key={index}>
-                        <Image src={anime.images.webp?.image_url || anime.images.jpg.image_url} width={350} height={350} className="w-full h-full object-cover"/>
+                        <Image src={anime.images.webp?.image_url || anime.images.jpg.image_url} width={350} height={350} className="w-full max-h-full object-cover"/>
                         <div className="absolute bottom-0 w-full text-center p-2 box-border overlay">
                           <h3 className="font-bold md:text-x text-md text-start  pb-0 whitespace-nowrap overflow-hidden text-ellipsis">{anime.title}</h3>
                           <p className="whitespace-normal md:text-sm text-xs text-start">score: {anime.score}</p>
@@ -24,7 +24,7 @@ const IndCharacterList = ({api}) => {
             {api.data?.map((character, index) => {
                 return (
                     <Link href={`/character/${character.mal_id}`}className="relative overflow-hidden cursor-pointer text-color-primary hover:text-color-accent transition-all anime rounded-md" key={index}>
-                        <Image src={character.images.webp?.image_url || character.images.jpg.image_url} width={350} height={350} className="w-full h-full object-cover"/>
+                        <Image src={character.images.webp?.image_url || character.images.jpg.image_url} width={350} height={350} className="w-full max-h-full object-cover"/>
                         <div className="absolute bottom-0 w-full text-center p-2 box-border overlay">
                           <h3 className="font-bold md:text-x text-md text-start  pb-0 whitespace-nowrap overflow-hidden text-ellipsis">{character.name}</h3>
                           <p className="whitespace-normal md:text-sm text-xs text-start">favorit: {character.favorites}</p>
