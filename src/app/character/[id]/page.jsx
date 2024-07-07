783080393124bc39107f7ec350d485a66dbc29b5
Blythes <145576625+Maousama-boop@users.noreply.github.com>
2024-07-07 10:42:34 +0700

    update src/app/character/[id]/page.jsx

--- src/app/character/[id]/page.jsx
+++ src/app/character/[id]/page.jsx
@@ -1,30 +1,23 @@
 import { getAnimeResponse } from "../../libs/api-libs"
 import Image from "next/image"
+import Link from "next/link"
 
-const Page = async ({ params: { id } }) => {
+const Page = async({params: {id}}) => {
     const animeCharacter = await getAnimeResponse(`characters/${id}`)
     return (
         <>
-            {animeCharacter.data?.map((character) => (
-                <div key={character.mal_id}>
-                    <div className="p-3 pb-0 font-semibold">
-                        <h3 className="text-color-primary text-2xl">{character.name}</h3>
-                        {character && character.name_kanji ? (
-                            <h4 className="text-color-primary text-l">{character.name_kanji}</h4>
-                        ) : null}
-                    </div>
-                    <div className="p-3 flex justify-center items-center sm:flex-nowrap flex-wrap gap-3 text-color-primary font-semibold capitalize">
-                        <Image 
-                            src={character.images.webp?.image_url || character.images.jpg.image_url} 
-                            height={250} 
-                            width={250} 
-                            alt={character.name}
-                            className="object-cover rounded-md w-48 md:w-56 h-full" 
-                        />
-                        <p className="text-justify sm:text-x text-sm">{character.about}</p>
-                    </div>
-                </div>
-            ))}
+            <div className="p-3 pb-0 font-semibold">
+                <h3 className="text-color-primary text-2xl">{animeCharacter.data.name}</h3>
+                {animeCharacter && animeCharacter.data.name_kanji ?
+                <h4 className="text-color-primary text-l">{animeCharacter.data.name_kanji}</h4>
+                :
+                null
+                }
+            </div>
+            <div className="p-3 flex justify-center items-center sm:flex-nowrap flex-wrap gap-3 text-color-primary font-semibold capitalize">
+            <Image src={animeCharacter.data.images.webp?.image_url || animeCharacter.data.images.jpg.image_url}  alt={animeCharacter.data.name} height={250} width={250} className="object-cover rounded-md w-48 md:w-56 h-full"/>
+            <p className="text-justify sm:text-x text-sm">{animeCharacter.data.about}</p>
+            </div>
         </>
     )
 }
