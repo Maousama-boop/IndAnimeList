c3d5ddd507ad455737cc500f407a667e0e5b1b34
Blythes <145576625+Maousama-boop@users.noreply.github.com>
2024-07-07 17:07:28 +0700

    update src/components/IndAnimeList/index.jsx

--- src/components/IndAnimeList/index.jsx
+++ src/components/IndAnimeList/index.jsx
@@ -7,7 +7,7 @@ const IndAnimeList = ({api}) => {
             {api.data?.map((anime, index) => {
                 return (
                     <Link href={`/anime/${anime.mal_id}`}className="cursor-pointer text-color-primary hover:text-color-accent transition-all anime" key={index}>
-                        <Image src={anime.images.webp.image_url} width={350} height={350} className="w-full max-h-64 rounded object-cover"/>
+                        <Image src={anime.images.webp.image_url} alt={anime.images.jpg.image_url} width={350} height={350} className="w-full max-h-64 rounded object-cover"/>
                           <h3 className="font-bold md:text-x text-md text-center whitespace-nowrap overflow-hidden text-ellipsis">{anime.title}</h3>
                     </Link>
                 )    
