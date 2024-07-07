276e4c50b8420de826be595998c45ca09ee01a76
Blythes <145576625+Maousama-boop@users.noreply.github.com>
2024-07-07 17:54:57 +0700

    update src/components/IndAnimeList/index.jsx

--- src/components/IndAnimeList/index.jsx
+++ src/components/IndAnimeList/index.jsx
@@ -1,13 +1,27 @@
 import Image from "next/image"
 import Link from "next/link"
+import React from 'react';
 
-const IndAnimeList = ({api}) => {
+interface IndAnimeListProps {
+  api: {
+    data: Array<{
+      mal_id: number,
+      title: string,
+      images: {
+        webp: {
+          image_url: string,
+        },
+      },
+    }>
+  },
+}
+const IndAnimeList: React.RF<IndAnimeListProps> = ({api}) => {
     return (
         <div className="grid md:grid-cols-5 sm:grid-cols-3 grid-cols-2 gap-4 px-3">
             {api.data?.map((anime, index) => {
                 return (
                     <Link href={`/anime/${anime.mal_id}`}className="cursor-pointer text-color-primary hover:text-color-accent transition-all anime" key={index}>
-                        <Image src={anime.images.webp.image_url} alt={anime.images.jpg.image_url} priority width={350} height={350} className="w-full max-h-64 rounded object-cover"/>
+                        <Image src={anime.images.webp.image_url} alt="..." priority width={350} height={350} className="w-full max-h-64 rounded object-cover"/>
                           <h3 className="font-bold md:text-x text-md text-center whitespace-nowrap overflow-hidden text-ellipsis">{anime.title}</h3>
                     </Link>
                 )    
