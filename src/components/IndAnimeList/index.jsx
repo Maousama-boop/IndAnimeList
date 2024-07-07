0d1ecf01f9411600521153f23ea4940127f08eae
Blythes <145576625+Maousama-boop@users.noreply.github.com>
2024-07-07 16:46:39 +0700

    update src/components/IndAnimeList/index.jsx

--- src/components/IndAnimeList/index.jsx
+++ src/components/IndAnimeList/index.jsx
@@ -6,12 +6,9 @@ const IndAnimeList = ({api}) => {
         <div className="grid md:grid-cols-5 sm:grid-cols-3 grid-cols-2 gap-4 px-3">
             {api.data?.map((anime, index) => {
                 return (
-                    <Link href={`/anime/${anime.mal_id}`}className="relative overflow-hidden cursor-pointer text-color-primary hover:text-color-accent transition-all anime rounded-md" key={index}>
+                    <Link href={`/anime/${anime.mal_id}`}className="cursor-pointer text-color-primary hover:text-color-accent transition-all anime rounded-md" key={index}>
                         <Image src={anime.images.webp.image_url} width={350} height={350} className="w-full max-h-64 object-cover"/>
-                        <div className="absolute bottom-0 w-full text-center p-2 box-border overlay">
-                          <h3 className="font-bold md:text-x text-md text-start  pb-0 whitespace-nowrap overflow-hidden text-ellipsis">{anime.title}</h3>
-                          <p className="whitespace-normal md:text-sm text-xs text-start">score: {anime.score}</p>
-                        </div>
+                          <h3 className="font-bold md:text-x text-md text-center whitespace-nowrap overflow-hidden text-ellipsis">{anime.title}</h3>
                     </Link>
                 )    
             })}
