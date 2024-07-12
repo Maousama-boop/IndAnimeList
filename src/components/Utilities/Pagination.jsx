0fb08b4a1f6e55a04da3d19e2f2ce97a221c3e36
sekar0506 <sekaranindita.0506@gmail.com>
2024-07-12 16:12:29 +0700

    Update Pagination.jsx

--- src/components/Utilities/Pagination.jsx
+++ src/components/Utilities/Pagination.jsx
@@ -1,3 +1,5 @@
+import { CaretDoubleLeft, CaretDoubleRight, CaretLeft, CaretRight } from "@phosphor-icons/react"
+
 const Pagination = ({ page, lastPage, setPage}) => {
 
     const scrollAnimation = () => {
@@ -15,15 +17,30 @@ const Pagination = ({ page, lastPage, setPage}) => {
         setPage((prevState) => prevState - 1)
         scrollAnimation()
     }
+    const handleFirstPage = () => {
+        setPage((prevState) => prevState - page + 1)
+        scrollAnimation()
+    }
+    const handleLastPage = () => {
+        setPage((prevState) => prevState + lastPage - page)
+        scrollAnimation()
+    }
+    
     
     return (
-        <div className="flex justify-center items-center py-4 px-2 gap-5 text-color-primary">
+        <div className="flex justify-center items-center py-4 px-2 text-color-primary">
             {page <= 1 ? null :
-                <button onClick={handlePrevPage} className="transition-all hover:text-color-accent">Prev</button>
+                <button onClick={handleFirstPage} className="transition-all hover:text-color-accent"><CaretDoubleLeft size={32} /></button>
             }
-            <p>{page} Dari {lastPage}</p>
+            {page <= 1 ? null :
+                <button onClick={handlePrevPage} className="transition-all hover:text-color-accent"><CaretLeft size={32} /></button>
+            }
+            <p className="mx-5">{page} Dari {lastPage}</p>
             {page >= lastPage ? null :
-                <button onClick={handleNextPage} className="transition-all hover:text-color-accent">Next</button>
+                <button onClick={handleNextPage} className="transition-all hover:text-color-accent"><CaretRight size={32} /></button>
+            }
+             {page >= lastPage ? null :
+                <button onClick={handleLastPage} className="transition-all hover:text-color-accent"><CaretDoubleRight size={32} /></button>
             }
         </div>
     )
