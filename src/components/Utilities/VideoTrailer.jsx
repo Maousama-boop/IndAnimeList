c48efe9108ad9b634a8545447387b63a7b575b64
sekar0506 <sekaranindita.0506@gmail.com>
2024-07-12 16:12:32 +0700

    Create VideoTrailer.jsx

---
+++ src/components/Utilities/VideoTrailer.jsx
@@ -0,0 +1,47 @@
+"use client"
+
+import Youtube from "react-youtube"
+import { X, YoutubeLogo } from "@phosphor-icons/react"
+import { useState } from "react"
+
+const VideoTrailer = ({trailerId}) => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const handleTrailerButton = () => {
+        setIsOpen((prevState) => !prevState)
+    }
+
+    const option = {
+        width: "300",
+        height: "250"
+    }
+
+    const Player = () => {
+        return(
+            <div className="fixed bottom-0 right-0">
+                <button onClick={handleTrailerButton} className="text-color-primary hover:text-color-red transition-all float-right p-2 m-2 bg-color-secondary rounded-full"><X size={30} /></button>
+                <Youtube
+                videoId={trailerId} 
+                onReady={(event) => event.target.pauseVideo()}
+                opts={option}
+                onError={() => alert("Trailer Anime Tidak Ada. Coba Trailer Anime Yang Lain.")}
+                />
+            </div>
+        )
+    }
+
+    const PlayerOpenTrailer = () => {
+        return(
+            <button
+                onClick={handleTrailerButton}
+                className="fixed bottom-5 right-5 bg-color-secondary p-2 rounded-full text-color-primary hover:text-color-red transition-all"
+                >
+                <YoutubeLogo size={40} />
+            </button>
+        )
+    }
+
+    return isOpen ? <Player/> : <PlayerOpenTrailer/>
+}
+
+export default VideoTrailer
