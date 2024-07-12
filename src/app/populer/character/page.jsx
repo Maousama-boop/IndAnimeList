165aa0229543221b108ab4d1440acbe0ceb21f6c
sekar0506 <sekaranindita.0506@gmail.com>
2024-07-12 16:11:58 +0700

    Update page.jsx

--- src/app/populer/character/page.jsx
+++ src/app/populer/character/page.jsx
@@ -1,31 +1,32 @@
 "use client"
 
-import { useEffect, useState } from "react"
-import HeaderPopuler from "../../../components/Utilities/HeaderPopuler"
+import React, { useEffect, useState } from "react"
+import { getAnimeResponse } from "../../../libs/api-libs"
 import Pagination from "../../../components/Utilities/Pagination"
+import HeaderPopuler from "../../../components/Utilities/HeaderPopuler"
 import { IndCharacterList } from "../../../components/IndAnimeList"
-import { getAnimeResponse } from "../../libs/api-libs"
 
-const CharacterPopuler = () => {
+
+const Page = () => {
     const [page, setPage] = useState(1)
     const [topCharacter, setTopCharacter] = useState([])
 
     const fetchData = async() => {
-        const populerCharacter = await getAnimeResponse("top/characters", `page=${page}`)
+        const populerCharacter= await getAnimeResponse("top/characters", `page=${page}`)
         setTopCharacter(populerCharacter)
     }
 
     useEffect(() => {
         fetchData()
     }, [page])
-    
+
     return (
         <>
             <HeaderPopuler title={`Character Populer #${page}`}/>
-            <IndCharacterList api={topCharacter}/>
-            <Pagination page={page} lastPage={topCharacter.pagination?.last_visible_page} setPage={setPage}/> 
+            <IndCharacterList api={topCharacter} />
+            <Pagination page={page} lastPage={topCharacter.pagination?.last_visible_page} setPage={setPage} />
         </>
     )
 }
 
-export default CharacterPopuler
+export default Page
