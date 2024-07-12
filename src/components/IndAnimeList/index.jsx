012592ba219844de826e8220a833c1fad423790b
sekar0506 <sekaranindita.0506@gmail.com>
2024-07-12 16:22:38 +0700

    Update index.jsx

--- src/components/IndAnimeList/index.jsx
+++ src/components/IndAnimeList/index.jsx
@@ -7,7 +7,7 @@ const IndAnimeList = ({api}) => {
             {api.data?.map((anime) => {
                 return (
                     <Link href={`/anime/${anime.mal_id}`}className="relative overflow-hidden cursor-pointer text-color-primary hover:text-color-accent transition-all rounded-xl" key={anime.mal_id}>
-                        <Image src={anime.images.webp.image_url} alt="..." width={350} height={350} className="w-full h-full object-cover anime"/>
+                        <Image src={anime.images.webp.image_url} alt="..." width={350} height={350} priority className="w-full h-full object-cover anime"/>
                         <div className="absolute bottom-0 w-full text-center p-2 box-border overlay">
                           <p className="whitespace-normal md:text-sm text-base text-start">Rank #{anime.rank}</p>
                           <h3 className="md:text-x text-md text-start pb-0 whitespace-nowrap overflow-hidden text-ellipsis font-bold">{anime.title}</h3>
@@ -24,7 +24,7 @@ const IndCharacterList = ({api}) => {
             {api.data?.map((character) => {
                 return (
                     <Link href={`/character/${character.mal_id}`}className="relative overflow-hidden cursor-pointer text-color-primary hover:text-color-accent transition-all rounded-xl" key={character.mal_id}>
-                        <Image src={character.images.webp.image_url} alt="..." width={350} height={350} className="w-full h-full object-cover character"/>
+                        <Image src={character.images.webp.image_url} alt="..." width={350} height={350} priority className="w-full h-full object-cover character"/>
                         <div className="absolute bottom-0 w-full text-center p-2 box-border overlay">
                           <p className="whitespace-normal md:text-sm text-base text-start">favorit #{character.favorites}</p>
                           <h3 className="md:text-x text-md text-start  pb-0 whitespace-nowrap overflow-hidden text-ellipsis font-bold">{character.name}</h3>
