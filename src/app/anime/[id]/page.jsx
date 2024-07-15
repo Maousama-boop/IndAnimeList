4fc51577b924671a9b8322ce39eb6eb6333a82d7
sekar0506 <sekaranindita.0506@gmail.com>
2024-07-15 12:17:15 +0700

    Update page.jsx

--- src/app/anime/[id]/page.jsx
+++ src/app/anime/[id]/page.jsx
@@ -1,9 +1,11 @@
 import VideoTrailer from "../../../components/Utilities/VideoTrailer"
 import Image from "next/image"
-import { getAnimeResponse } from "../../../libs/api-libs"
+import { getAnimeResponse, getNestedDataResponse } from "../../../libs/api-libs"
 
 const Page = async({params: {id}}) => {
   const animeDetail = await getAnimeResponse(`anime/${id}`)
+  const animeCharacter = await getNestedDataResponse(`anime/${id}/characters`,'character')
+  console.log(animeCharacter)
     return (
         <>
             <div className="p-3 pb-0 font-semibold">
@@ -15,7 +17,7 @@ const Page = async({params: {id}}) => {
                 }
             </div>
             <div className="p-3 flex justify-center sm:flex-nowrap flex-wrap gap-3 text-color-primary font-semibold capitalize">
-            <Image src={animeDetail.data.images.webp.image_url} alt={animeDetail.data.images.jpg.image_url} height={250} width={250} className="object-cover rounded-xl w-52 md:w-56 h-full"/>
+            <Image src={animeDetail.data.images.webp.image_url} alt={animeDetail.data.images.jpg.image_url} height={250} width={250} className="object-cover rounded-xl w-52 md:w-56 h-full" priority/>
             <p className="text-justify sm:text-x text-sm">{animeDetail.data.synopsis}</p>
             </div>
             <div>
