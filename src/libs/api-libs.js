fb2684593524055a77c765825074a30f882ade5a
sekar0506 <sekaranindita.0506@gmail.com>
2024-07-15 12:17:29 +0700

    Update api-libs.js

--- src/libs/api-libs.js
+++ src/libs/api-libs.js
@@ -6,5 +6,15 @@ export const getAnimeResponse = async(resource, query) => {
 
 export const getNestedDataResponse = async(resource, objectProperty) => {
     const response = await getAnimeResponse(resource)
-    return response.data.flatMap(data => data.entry)
+    return response.data.flatMap(data => data[objectProperty])
+}
+
+export const reproduce = (data, gap) => {
+    const first = ~~(Math.random() * (data.length - gap) + 1)
+    const last = first + gap
+
+    const response = {
+        data: data.slice(first, last)
+    }
+    return response
 }
