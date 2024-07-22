ce839184fb3a1b21550d537053c29266a57aac7e
sekar0506 <sekaranindita.0506@gmail.com>
2024-07-22 16:37:13 +0700

    Update index.jsx

--- src/components/NavigationBar/index.jsx
+++ src/components/NavigationBar/index.jsx
@@ -1,12 +1,16 @@
 import Link from "next/link"
 import InputSearch from "./inputSearch"
+import UserButton from "./userButton"
 
 const NavBar = () => {
     return (
-        <header className="bg-color-accent text-color-primary">
+        <header className="bg-color-accent text-color-primary sticky top-0 z-10">
             <div className="flex md:flex-row flex-col text-center justify-between md:items-center p-5 gap-3">
-                <Link href="/" className="font-bold font-mono text-2xl">IndAnimeList</Link>
-                <InputSearch/>
+                <div className="flex md:justify-start justify-between items-center text-center gap-4">
+                    <Link href="/" className="font-bold font-mono md:text-2xl text-3xl">IndAnimeList</Link>
+                    <UserButton/>
+                </div>
+                    <InputSearch/>
             </div>
         </header>
     )
